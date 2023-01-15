86f7d6e9410bdda6c2fe96f15c57ee8594f3788c
Torbjørn Birch Moltu <t.b.moltu@lyse.net>
2023-01-15 18:11:46 +0100

    Make stopping threads work again
    Some threads would exit before they got any work,
    and others would never exit due to not being notified.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ extern crate sha2;
 extern crate thread_priority;
 
 mod available_buffers;
+use available_buffers::*;
 
 use std::{fs, io::Read, process::exit, thread};
 use std::num::{NonZeroU16, NonZeroUsize};
@@ -56,24 +57,30 @@ struct UsedBuffer {
     length: usize,
 }
 
-#[derive(Default, Debug)]
-struct FilePoolData {
-    stop: bool,
-    to_read: Vec<PathBuf>,
-    to_hash: Vec<(PathBuf, mpsc::Receiver<UsedBuffer>)>,
+#[derive(Debug)]
+struct Queue<T> {
+    queue: Vec<T>,
+    stop_now: bool,
+    stop_when_empty: bool,
+}
+impl<T> Default for Queue<T> {
+    fn default() -> Self {
+        Queue { queue: Vec::new(), stop_now: false, stop_when_empty: false }
+    }
 }
 
 #[derive(Debug)]
-struct FilePool {
-    data: Mutex<FilePoolData>,
-    wake_reader: Condvar,
-    wake_hasher: Condvar,
-    buffers: available_buffers::AvailableBuffers,
+struct Pools {
+    to_read: Mutex<Queue<PathBuf>>,
+    reader_waker: Condvar,
+    to_hash: Mutex<Queue<(PathBuf, mpsc::Receiver<UsedBuffer>)>>,
+    hasher_waker: Condvar,
+    buffers: AvailableBuffers,
 }
 
 fn read_file(
         file_path: &Path,  mut file: fs::File,
-        thread_name: &str,  pool: &FilePool,
+        thread_name: &str,  pool: &Pools,
 ) {
     let mut buf = pool.buffers.get_buffer(pool.buffers.max_single_buffer_size(), thread_name);
     let (tx, rx) = mpsc::channel();
@@ -106,20 +113,23 @@ fn read_file(
             }
         }
         if let Some(rx) = insert_rx.take() {
-            let mut lock = pool.data.lock().unwrap();
-            lock.to_hash.push((file_path.to_owned(), rx));
+            let mut lock = pool.to_hash.lock().unwrap();
+            lock.queue.push((file_path.to_owned(), rx));
             drop(lock);
-            pool.wake_hasher.notify_one();
+            pool.hasher_waker.notify_one();
         }
     }
     pool.buffers.return_buffer(buf);
 }
 
-fn read_files(pool: Arc<FilePool>, thread_name: String) {
-    let mut lock = pool.data.lock().unwrap();
+fn read_files(pool: Arc<Pools>, thread_name: String) {
+    let mut lock = pool.to_read.lock().unwrap();
 
     loop {
-        if let Some(path) = lock.to_read.pop() {
+        if lock.stop_now {
+            eprintln!("{} quit due to stop signal", thread_name);
+            break;
+        } else if let Some(path) = lock.queue.pop() {
             drop(lock);
             println!("{} reading {}", thread_name, path.display());
             let file = fs::File::open(&path).unwrap_or_else(|e| {
@@ -128,64 +138,76 @@ fn read_files(pool: Arc<FilePool>, thread_name: String) {
             });
 
             read_file(&path, file, &thread_name, &pool);
-            lock = pool.data.lock().unwrap();
-        } else if lock.stop {
+            lock = pool.to_read.lock().unwrap();
+        } else if lock.stop_when_empty {
+            eprintln!("{} quit due to no more work", thread_name);
             break;
         } else {
-            lock = pool.wake_reader.wait(lock).unwrap();
+            lock = pool.reader_waker.wait(lock).unwrap();
         }
     }
 }
 
-fn hash_files(pool: Arc<FilePool>, hasher_thread_number: u16) {
+fn hash_file(
+        file_path: PathBuf,  chunks: mpsc::Receiver<UsedBuffer>,
+        hasher: &mut sha2::Sha256,  thread_name: &str,
+        buffers: &AvailableBuffers) {
+    let buf = match chunks.try_recv() {
+        Ok(buf) => buf,
+        Err(mpsc::TryRecvError::Empty | mpsc::TryRecvError::Disconnected) => {
+            println!("{} is empty", file_path.display());
+            return;
+        },
+    };
+
+    println!("{} hashing {}", thread_name, file_path.display());
+    hasher.update(&buf.buffer[..buf.length]);
+    let mut size = buf.length;
+    buffers.return_buffer(buf.buffer);
+
+    for buf in chunks.into_iter() {
+        if buf.length == 0 {
+            // IO error
+            hasher.reset();
+            return;
+        }
+        hasher.update(&buf.buffer[..buf.length]);
+        size += buf.length;
+        buffers.return_buffer(buf.buffer);
+    }
+
+    let hash_result = hasher.finalize_reset();
+    println!("{} {} bytes {:#x}", file_path.display(), size, hash_result);
+}
+
+fn hash_files(pool: Arc<Pools>,  thread_name: String) {
     let mut hasher = Sha256::new();
-    let mut lock = pool.data.lock().unwrap();
+    let mut lock = pool.to_hash.lock().unwrap();
 
-    'get_file: loop {
-        if let Some((path, rx)) = lock.to_hash.pop() {
-            let buf = match rx.try_recv() {
-                Ok(buf) => buf,
-                Err(mpsc::TryRecvError::Empty | mpsc::TryRecvError::Disconnected) => {
-                    println!("{} is empty", path.display());
-                    continue 'get_file;
-                },
-            };
+    loop {
+        if lock.stop_now {
+            eprintln!("{} quit due to stop signal", thread_name);
+            break;
+        } else if let Some((path, rx)) = lock.queue.pop() {
             drop(lock);
-
-            println!("hasher thread {} hashing {}", hasher_thread_number, path.display());
-            hasher.update(&buf.buffer[..buf.length]);
-            let mut size = buf.length;
-            pool.buffers.return_buffer(buf.buffer);
-
-            for buf in rx.into_iter() {
-                if buf.length == 0 {
-                    // IO error
-                    hasher.reset();
-                    lock = pool.data.lock().unwrap();
-                    continue 'get_file;
-                }
-                hasher.update(&buf.buffer[..buf.length]);
-                size += buf.length;
-                pool.buffers.return_buffer(buf.buffer);
-            }
-
-            let hash_result = hasher.finalize_reset();
-            println!("{} {} bytes {:#x}", path.display(), size, hash_result);
-            lock = pool.data.lock().unwrap();
-        } else if lock.stop {
+            hash_file(path, rx, &mut hasher, &thread_name, &pool.buffers);
+            lock = pool.to_hash.lock().unwrap();
+        } else if lock.stop_when_empty {
+            eprintln!("{} quit due to no more work", thread_name);
             break;
         } else {
-            lock = pool.wake_hasher.wait(lock).unwrap();
+            lock = pool.hasher_waker.wait(lock).unwrap();
         }
     }
 }
 
 fn main() {
     let args = Args::parse();
-    let pool = Arc::new(FilePool {
-        data: Mutex::new(FilePoolData::default()),
-        wake_reader: Condvar::new(),
-        wake_hasher: Condvar::new(),
+    let pool = Arc::new(Pools {
+        to_read: Mutex::new(Queue::default()),
+        reader_waker: Condvar::new(),
+        to_hash: Mutex::new(Queue::default()),
+        hasher_waker: Condvar::new(),
         buffers: available_buffers::AvailableBuffers::new(
             match args.max_buffers_memory {
                 Some(size) => usize::from(size).saturating_mul(1024*1024),
@@ -207,9 +229,10 @@ fn main() {
 
     let mut hasher_threads = Vec::with_capacity(u16::from(args.hasher_threads).into());
     for n in (1..=args.hasher_threads.into()).into_iter() {
+        let thread_name = format!("hasher_{}", n);
         let pool = pool.clone();
         let builder = ThreadBuilder::default()
-            .name(format!("hasher_{}", n))
+            .name(thread_name.clone())
             .priority(ThreadPriority::Min);
         #[cfg(unix)]
         let builder = builder.policy(
@@ -219,7 +242,7 @@ fn main() {
             if let Err(e) = priority_result {
                 eprintln!("Failed lowering thread priority: {:?}", e);
             }
-            hash_files(pool, n)
+            hash_files(pool, thread_name)
         }).unwrap();
         hasher_threads.push(thread);
     }
@@ -263,20 +286,30 @@ fn main() {
                 println!("{} is a {}, skipping.", entry_path.display(), file_type);
                 continue;
             }
-            let mut lock = pool.data.lock().unwrap();
-            lock.to_read.push(entry_path);
+            let mut lock = pool.to_read.lock().unwrap();
+            lock.queue.push(entry_path);
             drop(lock);
-            pool.wake_reader.notify_one();
+            pool.reader_waker.notify_one();
         }
     }
 
-    let mut lock = pool.data.lock().unwrap();
-    lock.stop = true;
+    // tell IO threads they can stop now
+    let mut lock = pool.to_read.lock().unwrap();
+    lock.stop_when_empty = true;
     drop(lock);
+    pool.reader_waker.notify_all();
     for thread in io_threads {
+        eprintln!("joining reader");
         thread.join().unwrap();
     }
+
+    // tell hashers they can stop now
+    let mut lock = pool.to_hash.lock().unwrap();
+    lock.stop_when_empty = true;
+    drop(lock);
+    pool.hasher_waker.notify_all();
     for thread in hasher_threads {
+        eprintln!("joining hasher");
         thread.join().unwrap();
     }
 }
